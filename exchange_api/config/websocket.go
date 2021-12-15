a2291a3082e6f67f4fd179ad1c6a11170df25ab3
tsewell <18008072409@163.com>
2021-12-15 09:24:58 +0800

    Update websocket.go

--- exchange_api/config/websocket.go
+++ exchange_api/config/websocket.go
@@ -1,9 +1,9 @@
 package config
 
 var (
-	OKEX_DEPTH_TABLE_SPOT = "spot/depth5"
+	OKEX_DEPTH_TABLE_SPOT 	= "spot/depth5"
 	OKEX_DEPTH_TABLE_FUTURE = "futures/depth5"
 
-	SPOT = "0" // 现货标识
-	FUTURE = "1" // 期货标识
+	SPOT 	= "0" // 现货标识
+	FUTURE 	= "1" // 期货标识
 )
